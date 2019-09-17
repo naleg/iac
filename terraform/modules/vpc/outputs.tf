9a54475c9c1e51df74e219f3974b5551e97a2c04
rajesh <gudur@pega.com>
2019-09-17 12:48:27 +0530

    adding types to outputs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
+  type  = "string"
   value = "${aws_vpc.main.id}"
 }
 
 output "public_subnets" {
+  type  = "list"
   value = "${aws_subnet.public.*.id}"
 }
 
 output "private_subnets" {
+  type  = "list"
   value = "${aws_subnet.private.*.id}"
 }
