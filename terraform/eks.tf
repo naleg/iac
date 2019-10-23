7dfab77c968e95970fc1d6f96449b7e354c92956
rajesh <Rajesh.Guduguntla@in.pega.com>
2019-10-23 12:42:35 +0530

    eks single asg

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -105,6 +105,27 @@ output "kubeconfig" {
   value = "${local.kubeconfig}"
 }
 
+locals {
+  configmap = <<CONFIGMAP
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.worker-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+
+CONFIGMAP
+}
+
+output "configmap" {
+  value = "${local.configmap}"
+}
 
 
 resource "aws_iam_role" "worker-node" {
@@ -128,17 +149,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role = "${aws_iam_role.worker-node.name}"
+  role       = "${aws_iam_role.worker-node.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role = "${aws_iam_role.worker-node.name}"
+  role       = "${aws_iam_role.worker-node.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = "${aws_iam_role.worker-node.name}"
+  role       = "${aws_iam_role.worker-node.name}"
 }
 
 resource "aws_iam_instance_profile" "worker-node" {
@@ -147,61 +168,61 @@ resource "aws_iam_instance_profile" "worker-node" {
 }
 
 resource "aws_security_group" "worker-node" {
-  name = "${var.deployment_name}_eks_worker"
+  name        = "${var.deployment_name}_eks_worker"
   description = "Security group for all nodes in the cluster"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = "${merge(var.common_tags, map(
-     "Name", "${var.deployment_name}_eks_worker",
-     "kubernetes.io/cluster/${aws_eks_cluster.cluster.name}", "owned",
-    ))}"
+    "Name", "${var.deployment_name}_eks_worker",
+    "kubernetes.io/cluster/${aws_eks_cluster.cluster.name}", "owned",
+  ))}"
 }
 
 resource "aws_security_group_rule" "worker-node-ingress-self" {
-  description = "Allow node to communicate with each other"
-  from_port = 0
-  protocol = "-1"
-  security_group_id = "${aws_security_group.worker-node.id}"
+  description              = "Allow node to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = "${aws_security_group.worker-node.id}"
   source_security_group_id = "${aws_security_group.worker-node.id}"
-  to_port = 65535
-  type = "ingress"
+  to_port                  = 65535
+  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "worker-node-ingress-cluster" {
-  description = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
-  from_port = 1025
-  protocol = "tcp"
-  security_group_id = "${aws_security_group.worker-node.id}"
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.worker-node.id}"
   source_security_group_id = "${aws_security_group.EKSMaster.id}"
-  to_port = 65535
-  type = "ingress"
+  to_port                  = 65535
+  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "cluster-ingress-node-https" {
-  description = "Allow pods to communicate with the cluster API Server"
-  from_port = 443
-  protocol = "tcp"
-  security_group_id = "${aws_security_group.EKSMaster.id}"
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.EKSMaster.id}"
   source_security_group_id = "${aws_security_group.worker-node.id}"
-  to_port = 443
-  type = "ingress"
+  to_port                  = 443
+  type                     = "ingress"
 }
 
 data "aws_ami" "eks-worker" {
   filter {
-    name = "name"
+    name   = "name"
     values = ["amazon-eks-node-${aws_eks_cluster.cluster.version}-v*"]
   }
 
   most_recent = true
-  owners = ["602401143452"] # Amazon EKS AMI Account ID
+  owners      = ["602401143452"] # Amazon EKS AMI Account ID
 }
 data "aws_region" "current" {}
 
@@ -215,12 +236,12 @@ USERDATA
 
 resource "aws_launch_configuration" "worker-node" {
   associate_public_ip_address = true
-  iam_instance_profile        = "${aws_iam_instance_profile.worker-node.name}"
-  image_id                    = "${data.aws_ami.eks-worker.id}"
-  instance_type               = "c5.large"
-  name_prefix                 = "${var.deployment_name}_eks_worker"
-  security_groups             = ["${aws_security_group.worker-node.id}"]
-  user_data_base64            = "${base64encode(local.worker-node-userdata)}"
+  iam_instance_profile = "${aws_iam_instance_profile.worker-node.name}"
+  image_id = "${data.aws_ami.eks-worker.id}"
+  instance_type = "c5.large"
+  name_prefix = "${var.deployment_name}_eks_worker"
+  security_groups = ["${aws_security_group.worker-node.id}"]
+  user_data_base64 = "${base64encode(local.worker-node-userdata)}"
 
   lifecycle {
     create_before_destroy = true
@@ -228,22 +249,22 @@ resource "aws_launch_configuration" "worker-node" {
 }
 
 resource "aws_autoscaling_group" "worker" {
-  desired_capacity     = 2
+  desired_capacity = 2
   launch_configuration = "${aws_launch_configuration.worker-node.id}"
-  max_size             = 2
-  min_size             = 1
-  name                 = "${var.deployment_name}_eks_worker"
-  vpc_zone_identifier  = "${module.vpc.public_subnets}"
+  max_size = 2
+  min_size = 1
+  name = "${var.deployment_name}_eks_worker"
+  vpc_zone_identifier = "${module.vpc.public_subnets}"
 
   tag {
-    key                 = "Name"
-    value               = "${var.deployment_name}_eks_worker"
+    key = "Name"
+    value = "${var.deployment_name}_eks_worker"
     propagate_at_launch = true
   }
 
   tag {
-    key                 = "kubernetes.io/cluster/${aws_eks_cluster.cluster.name}"
-    value               = "owned"
+    key = "kubernetes.io/cluster/${aws_eks_cluster.cluster.name}"
+    value = "owned"
     propagate_at_launch = true
   }
 }
